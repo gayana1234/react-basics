3792e25567bd5097643e1432e9cda8f8b0e0b495
Gayana <gayanaachari04@gmail.com>
2024-12-21 05:42:41 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Mynewcomp from './Gayana/Mynewcomp'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Gayana</h1>
+      <Mynewcomp/>
     </div>
   );
 }
---
+++ src/Gayana/Mynewcomp.js
@@ -0,0 +1,9 @@
+
+const Mynewcomp = () => {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+export default Mynewcomp
